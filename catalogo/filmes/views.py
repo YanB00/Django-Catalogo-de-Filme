acf4ca2a4702849a4b78dd5d241dedbb89a2db0e
unknown <ybarbosa648@gmail.com>
2025-06-19 19:42:36 -0300

    correção

--- catalogo/filmes/views.py
+++ catalogo/filmes/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Filme
+from .forms import FilmeForm
 
 def lista_filmes(request):
     filmes = Filme.objects.all()
     return render(request, 'filmes/lista.html', {'filmes': filmes})
+
+def criar_filme(request):
+    if request.method == 'POST':
+        form = FilmeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_filmes')
+    else:
+        form = FilmeForm()
+        
+    return render(request, 'filmes/form_filme.html', {'form':form})
+
+def atualizar_filme(request,pk):
+    filme = get_object_or_404(Filme, pk=pk)
+    if request.method == 'POST':
+        form = FilmeForm(request.POST, request.FILES, instance=filme)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_filmes')
+    else:
+        form = FilmeForm(instance=filme)
+        
+    return render(request, 'filmes/form_filme.html', {'form':form})
+
+def deletar_filme(request,pk):
+    filme = get_object_or_404(Filme, pk=pk)
+    if request.method == 'POST':
+        filme.delete()
+        return redirect('lista_filmes')        
+    return render(request, 'filmes/confirmar_exclusao.html', {'filme':filme})
+
+
